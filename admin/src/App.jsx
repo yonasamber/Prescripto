dea2b34f2ca188c08a6c6f8ab6bb4543bcc18d53
yonasamber <yonasamber@gmail.com>
2026-03-11 17:57:53 +0300

    add AddDoctor

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,10 @@ import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -17,6 +21,10 @@ const App = () => {
         <Sidebar />
         <Routes>
           <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
         </Routes>
       </div>
     </div>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AdminContext } from "../context/AdminContext";
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { aToken } = useContext(AdminContext);
+
+  return (
+    <div className="min-h-screen bg-white border-r">
+      {aToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            to={"/admin-dashboard"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+          >
+            <img src={assets.home_icon} alt="" />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink
+            to={"/all-appointments"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+          >
+            <img src={assets.appointment_icon} alt="" />
+            <p>Appointments</p>
+          </NavLink>
+          <NavLink
+            to={"/add-doctor"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+          >
+            <img src={assets.add_icon} alt="" />
+            <p>Add Doctor</p>
+          </NavLink>
+          <NavLink
+            to={"/doctor-list"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+          >
+            <img src={assets.people_icon} alt="" />
+            <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
